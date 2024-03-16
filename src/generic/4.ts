db838ae58be229b69da35943f9c221274708940c
Elizabeth Diachenko <liza.diachenko.99@gmail.com>
2024-03-16 10:31:57 +0200

    generics task 4

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,15 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
-
-  }
+interface PageType {
+  title: string;
+}
+class Component<T> {
+  constructor(public props: T) {}
 }
 
-class Page extends Component {
-  pageInfo () {
+class Page extends Component<PageType> {
+  pageInfo() {
     console.log(this.props.title);
   }
 }
